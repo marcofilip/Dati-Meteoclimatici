9e325feb26650bba28dd83998ce3a1bb22572d24
marcofilip <129083737+marcofilip@users.noreply.github.com>
2024-05-21 11:07:19 +0000

    Excel to Json method, converting json to graph data

--- dati-meteoclimatici/src/views/HomeView.vue
+++ dati-meteoclimatici/src/views/HomeView.vue
@@ -1,44 +1,108 @@
 <template>
   <div class="chart-container">
-    <apexchart type="bar" :options="chartOptions" :series="series"></apexchart>
+    <apexchart type="line" :options="chartOptionsTemperatura" :series="seriesTemperatura"></apexchart>
+    <h1>------</h1>
+    <apexchart type="line" :options="chartOptionsPrecipitazioni" :series="seriesPrecipitazioni"></apexchart>
   </div>
 </template>
 
 <script>
-import XlsxPopulate from 'xlsx-populate/browser/xlsx-populate';
-import VueApexCharts from 'vue3-apexcharts';
 
+import VueApexCharts from 'vue3-apexcharts';
+const XLSX = require('xlsx');
 export default {
   components: {
     apexchart: VueApexCharts,
   },
   data() {
     return {
-      chartOptions: {
+      chartOptionsTemperatura: {
         chart: {
-          id: 'vuechart-example'
+          id: 'Dati Meteoclimatici'
         },
         xaxis: {
           categories: []
         }
       },
-      series: [{
-        name: 'series-1',
+      seriesTemperatura: [{
+        name: 'Serie',
         data: []
       }],
+
+      chartOptionsPrecipitazioni: {
+        chart: {
+          id: 'Dati Meteoclimatici'
+        },
+        xaxis: {
+          categories: []
+        }
+      },
+      seriesPrecipitazioni: [{
+        name: 'Serie',
+        data: []
+      }],
+
+      datajson: [],
     };
   },
   async created() {
-    const response = await fetch('../../public/tabelle/tabelle-dati-meteoclimatici.xlsx');
-    const arrayBuffer = await response.arrayBuffer();
-    XlsxPopulate.fromDataAsync(arrayBuffer)
-      .then(workbook => {
-        const sheet = workbook.sheet(0);
-        const jsonData = sheet.usedRange().value();
-        this.chartOptions.xaxis.categories = jsonData.map(row => row[0]);
-        this.series[0].data = jsonData.map(row => row[1]);
+    this.ExcelToJson('tabelle/tabelle-dati-meteoclimatici.xlsx')
+      .then(() => {
+        this.LoadGraph();
       });
   },
+
+  methods: {
+    async ExcelToJson(url) {
+      try {
+        const response = await fetch(url);
+        const data = await response.arrayBuffer();
+        const workbook = XLSX.read(data, { type: 'array' });
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+        this.datajson = jsonData;
+        console.log(this.datajson);
+      } catch (error) {
+        console.error('Error loading the Excel file:', error);
+      }
+    },
+
+    LoadGraph() {
+      const temperature = [];
+      const precipitazioni = [];
+      let temperatura, precipitazione;
+
+      for (let i = 0; i < 16; i++) {
+
+        if (i === 0) {
+          temperatura = this.datajson[1][`__EMPTY`];
+        }
+        else {
+          temperatura = this.datajson[1][`__EMPTY_${i}`];
+        }
+
+        temperature.push(temperatura);
+      }
+      for (let i = 17; i < 33; i++) {
+
+        if (i === 0) {
+          precipitazione = this.datajson[1][`__EMPTY`];
+        }
+        else {
+          precipitazione = this.datajson[1][`__EMPTY_${i}`];
+        }
+
+        precipitazioni.push(precipitazione);
+      }
+
+      this.chartOptionsTemperatura.xaxis.categories = temperature;
+      this.chartOptionsPrecipitazioni.xaxis.categories = precipitazioni;
+
+      console.log("Categorie Temperature: ", this.chartOptionsTemperatura.xaxis.categories);
+      console.log("Categorie Precipitazioni: ", this.chartOptionsPrecipitazioni.xaxis.categories);
+    },
+  }
 };
 </script>
 
